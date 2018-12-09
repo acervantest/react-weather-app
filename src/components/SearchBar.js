b42a623c7c9471f4a5bc23065c1bbe31ad1a3062
alejandro <alejandro@alejandros-MacBook-Air.local>
2018-12-09 08:18:34 -0800

    Ajax request implementation using Axios and Redux

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component{
+  class SearchBar extends Component{
 
   constructor(props){
       super(props);
@@ -10,15 +13,23 @@ export default class SearchBar extends Component{
       }
 
       this.onInputChange = this.onInputChange.bind(this);
+      this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(e){
       this.setState({ term: event.target.value });
   }
 
+  onFormSubmit(e){
+    e.preventDefault();
+
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: ''});
+  }
+
   render() {
     return(
-      <form className="input-group">
+      <form onSubmit={this.onFormSubmit} className="input-group">
         <input
           placeholder="Get five-day forecast in your favorite cities"
           className="form-control"
@@ -32,3 +43,10 @@ export default class SearchBar extends Component{
     );
   }
 }
+
+
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
